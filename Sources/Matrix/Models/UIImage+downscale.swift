c4b97ce8e43d9989606569d686bc8debad3e4b5a
Charles Wright <cvwright@futo.org>
2022-10-27 16:43:04 -0500

    Adding our old `downscale_image` utility function as an extension to UIImage

---
+++ Sources/Matrix/Models/UIImage+downscale.swift
@@ -0,0 +1,39 @@
+//
+//  UIImage+downscale.swift
+//  
+//
+//  Created by Charles Wright on 10/27/22.
+//
+
+import Foundation
+
+#if !os(macOS)
+
+extension UIImage {
+    func downscale(to maxSize: CGSize) -> UIImage? {
+        let height = self.size.height
+        let width = self.size.width
+        let MAX_HEIGHT = maxSize.height
+        let MAX_WIDTH = maxSize.width
+        print("DOWNSCALE\t h = \(height)\t w = \(width)")
+        print("DOWNSCALE\t max h = \(MAX_HEIGHT)\t max w = \(MAX_WIDTH)")
+
+        if height > MAX_HEIGHT || width > MAX_WIDTH {
+            let aspectRatio = self.size.width / self.size.height
+            print("DOWNSCALE\tAspect ratio = \(aspectRatio)")
+            let scale = aspectRatio > 1
+                ? MAX_WIDTH / self.size.width
+                : MAX_HEIGHT / self.size.height
+            print("DOWNSCALE\tScale = \(scale)")
+            let newSize = CGSize(width: scale*self.size.width, height: scale*self.size.height)
+            print("DOWNSCALE\tNew size = \(newSize)")
+            let renderer = UIGraphicsImageRenderer(size: newSize)
+            return renderer.image { (context) in
+                self.draw(in: CGRect(origin: .zero, size: newSize))
+            }
+        }
+        return self
+    }
+}
+
+#endif
