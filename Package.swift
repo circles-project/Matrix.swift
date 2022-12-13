c58f1ffaaf5f880d1e4aea3fd6821e984467ab17
Michael Hollister <michael@futo.org>
2022-12-13 09:33:51 -0600

    Added initial support for unit test configuration file

--- Package.swift
+++ Package.swift
@@ -48,6 +48,9 @@ let package = Package(
                 .product(name: "AnyCodable", package: "anycodable"),
                 .product(name: "BlindSaltSpeke", package: "blindsaltspeke"),
                 .product(name: "Yams", package: "yams"),
+            ],
+            resources: [
+                .copy("TestConfig.yaml")
             ]),
     ]
 )
