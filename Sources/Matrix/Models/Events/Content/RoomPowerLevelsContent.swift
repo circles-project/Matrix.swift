c919bea7b2c0eab81ffc335cf940e6e368fdbe4c
Charles Wright <cvwright@futo.org>
2023-02-10 12:53:04 -0600

    Making everything in m.room.power_levels optional.  Turns out nothing in particular is required.

--- Sources/Matrix/Models/Events/Content/RoomPowerLevelsContent.swift
+++ Sources/Matrix/Models/Events/Content/RoomPowerLevelsContent.swift
@@ -9,25 +9,31 @@ import Foundation
 
 /// m.room.power_levels: https://spec.matrix.org/v1.5/client-server-api/#mroompower_levels
 public struct RoomPowerLevelsContent: Codable {
-    public let invite: Int
-    public let kick: Int
-    public let ban: Int
+    public let invite: Int?
+    public let kick: Int?
+    public let ban: Int?
+    public let redact: Int?
     
-    public let events: [String: Int]
-    public let eventsDefault: Int
+    public let events: [Matrix.EventType: Int]?
+    public let eventsDefault: Int?
 
     public let notifications: [String: Int]?
     
-    public let redact: Int
-    
-    public let stateDefault: Int
+    public let stateDefault: Int?
 
-    public let users: [String: Int]
-    public let usersDefault: Int
+    public let users: [UserId: Int]?
+    public let usersDefault: Int?
     
-    public init(invite: Int, kick: Int, ban: Int, events: [String : Int], eventsDefault: Int,
-                notifications: [String : Int]?, redact: Int, stateDefault: Int, users: [String : Int],
-                usersDefault: Int) {
+    public init(invite: Int? = nil,
+                kick: Int? = nil,
+                ban: Int? = nil,
+                redact: Int? = nil,
+                events: [Matrix.EventType : Int]? = nil,
+                eventsDefault: Int? = nil,
+                notifications: [String : Int]? = nil,
+                stateDefault: Int? = nil,
+                users: [UserId : Int]? = nil,
+                usersDefault: Int? = nil) {
         self.invite = invite
         self.kick = kick
         self.ban = ban
