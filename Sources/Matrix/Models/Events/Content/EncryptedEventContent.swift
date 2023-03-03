485ea2c92c3b597b877c736b2c6f8221ad04d4fa
Charles Wright <cvwright@futo.org>
2023-03-03 16:58:54 -0600

    Cleanup ciphertext types, add plaintext types

--- Sources/Matrix/Models/Events/Content/EncryptedEventContent.swift
+++ Sources/Matrix/Models/Events/Content/EncryptedEventContent.swift
@@ -6,8 +6,35 @@
 //
 
 import Foundation
+import AnyCodable
 
 public protocol MatrixCiphertext: Codable {}
+
+public struct MegolmPlaintext: Codable {
+    public let type: String
+    public let content: Codable
+    public let roomId: RoomId
+    
+    public enum CodingKeys: String, CodingKey {
+        case type
+        case content
+        case roomId = "room_id"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.type = try container.decode(String.self, forKey: .type)
+        self.content = try Matrix.decodeEventContent(of: self.type, from: decoder)
+        self.roomId = try container.decode(RoomId.self, forKey: .roomId)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.type, forKey: .type)
+        try container.encode(AnyCodable(self.content), forKey: .content)
+        try container.encode(self.roomId, forKey: .roomId)
+    }
+}
 
 public struct MegolmCiphertext: MatrixCiphertext {
     public let base64: String
@@ -25,12 +52,34 @@ public struct MegolmCiphertext: MatrixCiphertext {
     }
 }
 
+public struct OlmPlaintext: Codable {
+    public let type: String
+    public let content: String // Codable ???
+    public let sender: UserId
+    public let recipient: UserId
+    public let recipientKeys: [String: String]
+    public let keys: [String: String]
+    
+    public enum CodingKeys: String, CodingKey {
+        case type
+        case content
+        case sender
+        case recipient
+        case recipientKeys = "recipient_keys"
+        case keys
+    }
+}
+
 public struct OlmCiphertext: MatrixCiphertext {
     public struct EncryptedPayload: Codable {
-        public let type: Int
+        public enum MessageType: Int, Codable {
+            case preKey = 0
+            case ordinary = 1
+        }
+        public let type: MessageType
         public let body: String
         
-        public init(type: Int, body: String) {
+        public init(type: MessageType, body: String) {
             self.type = type
             self.body = body
         }
@@ -52,21 +101,25 @@ public struct OlmCiphertext: MatrixCiphertext {
 
 public struct EncryptedEventContent: Codable {
     public let algorithm: EncryptionAlgorithm
-    public let senderKey: String
-    public let deviceId: String
-    public let sessionId: String
     public let ciphertext: MatrixCiphertext
+    public let deviceId: String?
+    public let senderKey: String?
+    public let sessionId: String?
     
     public enum CodingKeys: String, CodingKey {
         case algorithm
-        case senderKey = "sender_key"
-        case deviceId = "device_id"
-        case sessionId = "session_id"
         case ciphertext
+        case deviceId = "device_id"
+        case senderKey = "sender_key"
+        case sessionId = "session_id"
     }
     
-    public init(algorithm: EncryptionAlgorithm, senderKey: String, deviceId: String, sessionId: String,
-                ciphertext: MatrixCiphertext) {
+    public init(algorithm: EncryptionAlgorithm,
+                ciphertext: MatrixCiphertext,
+                deviceId: String?,
+                senderKey: String?,
+                sessionId: String?
+    ) {
         self.algorithm = algorithm
         self.senderKey = senderKey
         self.deviceId = deviceId
@@ -78,40 +131,26 @@ public struct EncryptedEventContent: Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
         self.algorithm = try container.decode(EncryptionAlgorithm.self, forKey: .algorithm)
-        self.senderKey = try container.decode(String.self, forKey: .senderKey)
-        self.deviceId = try container.decode(String.self, forKey: .deviceId)
-        self.sessionId = try container.decode(String.self, forKey: .sessionId)
-        
         switch self.algorithm {
         case .olmV1:
             self.ciphertext = try container.decode(OlmCiphertext.self, forKey: .ciphertext)
         case .megolmV1:
             self.ciphertext = try container.decode(MegolmCiphertext.self, forKey: .ciphertext)
         }
+        
+        self.deviceId = try container.decodeIfPresent(String.self, forKey: .deviceId)
+        self.senderKey = try container.decodeIfPresent(String.self, forKey: .senderKey)
+        self.sessionId = try container.decodeIfPresent(String.self, forKey: .sessionId)
     }
     
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
         try container.encode(algorithm, forKey: .algorithm)
-        try container.encode(senderKey, forKey: .senderKey)
-        try container.encode(deviceId, forKey: .deviceId)
-        try container.encode(sessionId, forKey: .sessionId)
         try container.encode(ciphertext, forKey: .ciphertext)
+        
+        try container.encodeIfPresent(deviceId, forKey: .deviceId)
+        try container.encodeIfPresent(senderKey, forKey: .senderKey)
+        try container.encodeIfPresent(sessionId, forKey: .sessionId)
     }
 }
-
-/*
-public struct EventPlaintextPayload: Codable {
-    public let type: String
-    public let content: MatrixMessageContent
-    public let roomId: String
-    
-    public init(from decoder: Decoder) throws {
-        // FIXME: Need to borrow code from MatrixClientEvent to decode this thing
-    }
-    
-    // As with MatrixClientEvent, support for .encode() and Encodable should be automatic, since the only ambiguity is on the input side.
-    // Once we have a Codable MatrixMessageContent, whatever it is, it knows how to encode itself.
-}
-*/
