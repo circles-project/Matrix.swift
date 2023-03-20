87ce22723f39c3ba8fb6de335c59cf607b638328
Charles Wright <cvwright@futo.org>
2023-03-20 18:58:37 -0500

    Added encryption status and `addReaction()`

--- Sources/Matrix/Models/Matrix+Message.swift
+++ Sources/Matrix/Models/Matrix+Message.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Matrix {
-    public class Message: ObservableObject {
+    public class Message: ObservableObject, Identifiable {
         private(set) public var event: ClientEventWithoutRoomId
         public var room: Room
         public var sender: User
@@ -16,6 +16,8 @@ extension Matrix {
         @Published public var thumbnail: NativeImage?
         @Published public var reactions: [String:UInt]
         public var blurhash: NativeImage?
+        
+        public var isEncrypted: Bool
         
         private var fetchThumbnailTask: Task<Void,Swift.Error>?
         
@@ -26,10 +28,17 @@ extension Matrix {
             self.reactions = [:]
             
             // FIXME: Initialize the blurhash
+            
+            self.isEncrypted = event.type == M_ROOM_ENCRYPTED
+            // FIXME: Now try to decrypt???
         }
         
         public var eventId: EventId {
             event.eventId
+        }
+        
+        public var id: String {
+            "\(eventId)"
         }
         
         public var roomId: RoomId {
@@ -40,15 +49,11 @@ extension Matrix {
             event.type
         }
         
-        public var content: Codable {
-            event.content
+        public var content: MessageContent {
+            event.content as! MessageContent
         }
         
         public var mimetype: String? {
-            guard let content = event.content as? MessageContent
-            else {
-                return nil
-            }
             return content.mimetype
         }
         
@@ -101,6 +106,10 @@ extension Matrix {
                 }
                 self.fetchThumbnailTask = nil
             }
+        }
+        
+        public func addReaction(_ reaction: String) async throws -> EventId {
+            try await self.room.addReaction(reaction, to: eventId)
         }
     }
 }
