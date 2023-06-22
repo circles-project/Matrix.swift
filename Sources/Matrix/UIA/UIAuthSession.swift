a5fb9b1d9435d54a5a7d4903cffee980dad16a64
Charles Wright <cvwright@futo.org>
2023-06-22 10:43:51 -0500

    Moved the constants for UIA into their own file....  Maybe these should go into the main Constants.swift file, I'm not sure...

--- Sources/Matrix/UIA/UIAuthSession.swift
+++ Sources/Matrix/UIA/UIAuthSession.swift
@@ -9,20 +9,6 @@ import Foundation
 import AnyCodable
 import BlindSaltSpeke
 
-public let AUTH_TYPE_ENROLL_BSSPEKE_OPRF = "m.enroll.bsspeke-ecc.oprf"
-public let AUTH_TYPE_ENROLL_BSSPEKE_SAVE = "m.enroll.bsspeke-ecc.save"
-public let AUTH_TYPE_TERMS = "m.login.terms"
-public let AUTH_TYPE_ENROLL_PASSWORD = "m.enroll.password"
-public let AUTH_TYPE_DUMMY = "m.login.dummy"
-public let AUTH_TYPE_ENROLL_EMAIL_REQUEST_TOKEN = "m.enroll.email.request_token"
-public let AUTH_TYPE_ENROLL_EMAIL_SUBMIT_TOKEN = "m.enroll.email.submit_token"
-
-public let AUTH_TYPE_LOGIN_PASSWORD = "m.login.password"
-public let AUTH_TYPE_LOGIN_BSSPEKE_OPRF = "m.login.bsspeke-ecc.oprf"
-public let AUTH_TYPE_LOGIN_BSSPEKE_VERIFY = "m.login.bsspeke-ecc.verify"
-public let AUTH_TYPE_LOGIN_EMAIL_REQUEST_TOKEN = "m.login.email.request_token"
-public let AUTH_TYPE_LOGIN_EMAIL_SUBMIT_TOKEN = "m.login.email.submit_token"
-
 @available(macOS 12.0, *)
 public class UIAuthSession<T: Codable>: UIASession, ObservableObject {
     
