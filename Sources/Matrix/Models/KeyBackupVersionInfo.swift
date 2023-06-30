e3c060cd6f08ac0475bcfe1b67fa3ec44ede053d
Charles Wright <cvwright@futo.org>
2023-06-29 19:11:15 -0500

    Forgot to include the CodingKeys

--- Sources/Matrix/Models/KeyBackupVersionInfo.swift
+++ Sources/Matrix/Models/KeyBackupVersionInfo.swift
@@ -9,6 +9,25 @@ import Foundation
 
 extension Matrix {
     
+    
+    /* Example:
+     {
+        "version": "3",
+        "algorithm": "m.megolm_backup.v1.curve25519-aes-sha2",
+        "auth_data": {
+            "public_key": "Mrxfu+9SZktqomZ4MDryY63Q7wHhsXf4HinP47Pg6xg",
+            "signatures": {
+                "@test062909:us.circles-dev.net": {
+                    "ed25519:ihS/b7e2vZB0uouVi4NW6acbRpiES3iVHX0pajJsldg":"WQ7fYZB6HOtLqr6ujwNOihujj3QGDyo/hLIPmSdkDhG+cHf4ukMgBELFy9XQ/5iZtYHNj/WsBtKnYkWp3RthDw",
+                    "ed25519:ENLVRXXYFB":"GN/UnV7LkUTfauEaalgNQm7VXm8evr3LppNZiFFe5i9oNif+peuBPdRTpO0ZRxsBt+1W56+QYFoi0cMXHRmXAA"
+                }
+            }
+        },
+        "etag": "0",
+        "count": 0
+     }
+     */
+    
     public struct KeyBackupVersionInfo: Codable {
         
         // NOTE: This is only for Megolm backup v1
@@ -17,6 +36,11 @@ extension Matrix {
         struct AuthData: Codable {
             var publicKey: String
             var signatures: [String: [String:String]]?
+            
+            enum CodingKeys: String, CodingKey {
+                case publicKey = "public_key"
+                case signatures
+            }
         }
         
         var algorithm: String
