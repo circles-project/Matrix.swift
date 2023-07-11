395b08ed210f28805e86ffb9dfb4b2b3291ebc35
Charles Wright <cvwright@futo.org>
2023-07-11 11:00:03 -0500

    Move the AccountDataEvent into its own file

---
+++ Sources/Matrix/Models/Events/AccountDataEvent.swift
@@ -0,0 +1,36 @@
+//
+//  AccountDataEvent.swift
+//  
+//
+//  Created by Charles Wright on 7/11/23.
+//
+
+import Foundation
+
+extension Matrix {
+    public struct AccountDataEvent: Codable {
+        public var type: String
+        public var content: Codable
+        
+        public enum CodingKeys: String, CodingKey {
+            case type
+            case content
+        }
+        
+        public init(from decoder: Decoder) throws {
+            logger.debug("Decoding account data event")
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            let type = try container.decode(String.self, forKey: .type)
+            self.type = type
+            logger.debug("\tAccount data event type = \(type)")
+            self.content = try Matrix.decodeAccountData(of: self.type, from: decoder)
+        }
+        
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(type, forKey: .type)
+            try container.encode(content, forKey: .content)
+        }
+    }
+}
--- Sources/Matrix/Models/SyncResponseBody.swift
+++ Sources/Matrix/Models/SyncResponseBody.swift
@@ -9,32 +9,6 @@ import Foundation
 
 extension Matrix {
     
-    public struct AccountDataEvent: Codable {
-        public var type: String
-        public var content: Codable
-        
-        public enum CodingKeys: String, CodingKey {
-            case type
-            case content
-        }
-        
-        public init(from decoder: Decoder) throws {
-            logger.debug("Decoding account data event")
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            
-            let type = try container.decode(String.self, forKey: .type)
-            self.type = type
-            logger.debug("\tAccount data event type = \(type)")
-            self.content = try Matrix.decodeAccountData(of: self.type, from: decoder)
-        }
-        
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            try container.encode(type, forKey: .type)
-            try container.encode(content, forKey: .content)
-        }
-    }
-    
     public struct SyncResponseBody: Decodable {
         public struct MinimalEventsContainer: Decodable {
             public var events: [MinimalEvent]?
