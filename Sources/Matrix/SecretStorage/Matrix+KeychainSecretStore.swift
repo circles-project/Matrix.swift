479d7a21070aba1d24b8073c8c6ed1656855afc6
Charles Wright <cvwright@futo.org>
2023-11-16 10:45:48 -0600

    Split the different keychain implementations into different classes that all implement the same protocol

--- Sources/Matrix/SecretStorage/Matrix+KeychainSecretStore.swift
+++ Sources/Matrix/SecretStorage/Matrix+KeychainSecretStore.swift
@@ -10,19 +10,46 @@ import os
 import LocalAuthentication
 import KeychainAccess
 
+public protocol KeyStoreProtocol {
+    init(userId: UserId)
+    func loadKey(keyId: String, reason: String) async throws -> Data?
+    func saveKey(key: Data, keyId: String) async throws
+}
+
 extension Matrix {
-    public class KeychainSecretStore {
+    
+    public class InsecureKeyStore: KeyStoreProtocol {
+        let userId: UserId
+        private var logger: os.Logger
+
+        public required init(userId: UserId) {
+            self.userId = userId
+            self.logger = .init(subsystem: "matrix", category: "insecure-keychain")
+        }
+        
+        public func loadKey(keyId: String, reason: String) async throws -> Data? {
+            logger.warning("WARNING TOTALLY INSECURE FAKE KEYCHAIN - FIXME")
+            return UserDefaults.standard.data(forKey: "org.futo.ssss.key.\(keyId)")
+        }
+        
+        public func saveKey(key: Data, keyId: String) async throws {
+            logger.warning("WARNING TOTALLY INSECURE FAKE KEYCHAIN - FIXME")
+            UserDefaults.standard.set(key, forKey: "org.futo.ssss.key.\(keyId)")
+        }
+    }
+    
+    public class KeychainAccessKeyStore: KeyStoreProtocol {
         let userId: UserId
         private var keychain: Keychain
         private var logger: os.Logger
-        
-        public init(userId: UserId) {
+
+        public required init(userId: UserId) {
             self.userId = userId
             self.keychain = Keychain(service: "matrix", accessGroup: userId.stringValue)
-            self.logger = .init(subsystem: "matrix", category: "keychain")
+            self.logger = .init(subsystem: "matrix", category: "keychainaccess")
         }
         
-        private func loadKey_KeychainAccess(keyId: String, reason: String) async throws -> Data? {
+        public func loadKey(keyId: String, reason: String) async throws -> Data? {
             // https://github.com/kishikawakatsumi/KeychainAccess#closed_lock_with_key-obtaining-a-touch-id-face-id-protected-item
             // Ensure this runs on a background thread - Otherwise if we try to authenticate to the keychain from the main thread, the app will lock up
             let t = Task(priority: .background) {
@@ -44,13 +71,38 @@ extension Matrix {
             return try await t.value
         }
         
-        private func loadKey_FakeKeychain(keyId: String, reason: String) async throws -> Data? {
-            logger.warning("WARNING TOTALLY INSECURE FAKE KEYCHAIN - FIXME")
-            return UserDefaults.standard.data(forKey: "org.futo.ssss.key.\(keyId)")
+        public func saveKey(key: Data, keyId: String) async throws {
+            // https://github.com/kishikawakatsumi/KeychainAccess#closed_lock_with_key-updating-a-touch-id-face-id-protected-item
+            // Ensure this runs on a background thread - Otherwise if we try to authenticate to the keychain from the main thread, the app will lock up
+            let t = Task(priority: .background) {
+                var context = LAContext()
+                context.touchIDAuthenticationAllowableReuseDuration = 60.0
+                self.logger.debug("Got context.  Attempting to save keyId \(keyId)")
+                try keychain
+                    .accessibility(.whenUnlockedThisDeviceOnly, authenticationPolicy: .userPresence)
+                    .authenticationContext(context)
+                    .set(key, key: keyId)
+                self.logger.debug("Success saving keyId \(keyId)")
+            }
+            try await t.value
+        }
+    }
+    
+    
+    public class KeychainKeyStore: KeyStoreProtocol {
+        let userId: UserId
+        private var keychain: Keychain
+        private var logger: os.Logger
+        
+        public required init(userId: UserId) {
+            self.userId = userId
+            self.keychain = Keychain(service: "matrix", accessGroup: userId.stringValue)
+            self.logger = .init(subsystem: "matrix", category: "keychain")
         }
         
+        
         // Use the Apple Keychain APIs directly, with no KeychainAccess
-        private func loadKey_RawKeychain(keyId: String, reason: String) async throws -> Data? {
+        public func loadKey(keyId: String, reason: String) async throws -> Data? {
             // https://developer.apple.com/documentation/security/keychain_services/keychain_items/searching_for_keychain_items
             let tag = "org.futo.ssss.key.\(keyId)".data(using: .utf8)! // From the Apple article on saving keys to keychain
             let query: [String: Any] = [kSecClass as String: kSecClassKey,
@@ -75,37 +127,7 @@ extension Matrix {
             return keyData
         }
         
-        public func loadKey(keyId: String, reason: String) async throws -> Data? {
-            // https://developer.apple.com/documentation/security/keychain_services/keychain_items/searching_for_keychain_items
-
-            logger.debug("Attempting to load key with keyId \(keyId)")
-            //return try await loadKey_KeychainAccess(keyId: keyId, reason: reason)
-            //return try await loadKey_RawKeychain(keyId: keyId, reason: reason)
-            return try await loadKey_FakeKeychain(keyId: keyId, reason: reason)
-        }
-        
-        public func saveKey_KeychainAccess(key: Data, keyId: String) async throws {
-            // https://github.com/kishikawakatsumi/KeychainAccess#closed_lock_with_key-updating-a-touch-id-face-id-protected-item
-            // Ensure this runs on a background thread - Otherwise if we try to authenticate to the keychain from the main thread, the app will lock up
-            let t = Task(priority: .background) {
-                var context = LAContext()
-                context.touchIDAuthenticationAllowableReuseDuration = 60.0
-                self.logger.debug("Got context.  Attempting to save keyId \(keyId)")
-                try keychain
-                    .accessibility(.whenUnlockedThisDeviceOnly, authenticationPolicy: .userPresence)
-                    .authenticationContext(context)
-                    .set(key, key: keyId)
-                self.logger.debug("Success saving keyId \(keyId)")
-            }
-            try await t.value
-        }
-        
-        public func saveKey_FakeKeychain(key: Data, keyId: String) async throws {
-            logger.warning("WARNING TOTALLY INSECURE FAKE KEYCHAIN - FIXME")
-            UserDefaults.standard.set(key, forKey: "org.futo.ssss.key.\(keyId)")
-        }
-        
-        public func saveKey_RawKeychain(key: Data, keyId: String) async throws {
+        public func saveKey(key: Data, keyId: String) async throws {
             // https://developer.apple.com/documentation/security/certificate_key_and_trust_services/keys/storing_keys_in_the_keychain
             logger.debug("Saving key with the raw Keychain API")
             
@@ -125,12 +147,5 @@ extension Matrix {
             logger.debug("Saved key \(keyId) in the keychain")
         }
         
-        public func saveKey(key: Data, keyId: String) async throws {
-            logger.debug("Attempting to save key with keyId \(keyId)")
-
-            //try await saveKey_KeychainAccess(key: key, keyId: keyId)
-            //try await saveKey_RawKeychain(key: key, keyId: keyId)
-            try await saveKey_FakeKeychain(key: key, keyId: keyId)
-        }
     }
 }
