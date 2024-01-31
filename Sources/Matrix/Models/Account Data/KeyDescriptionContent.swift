946df01c547f2f3c4afdcaab202fc50f7fce537c
Charles Wright <cvwright@futo.org>
2024-01-31 14:13:12 -0600

    Move KeyDescriptionContent under the Matrix namespace

--- Sources/Matrix/Models/Account Data/KeyDescriptionContent.swift
+++ Sources/Matrix/Models/Account Data/KeyDescriptionContent.swift
@@ -7,35 +7,36 @@
 
 import Foundation
 
-// https://spec.matrix.org/v1.6/client-server-api/#key-storage
-public struct KeyDescriptionContent: Codable {
-    public var name: String?
-    public var algorithm: String
-    public var passphrase: Passphrase?
-    public var iv: String?
-    public var mac: String?
-    
-    public struct Passphrase: Codable {
+extension Matrix {
+    // https://spec.matrix.org/v1.6/client-server-api/#key-storage
+    public struct KeyDescriptionContent: Codable {
+        public var name: String?
         public var algorithm: String
-        public var salt: String?    // Required for m.pbkdf2
-        public var iterations: Int? // Required for m.pbkdf2
-        public var bits: Int?
+        public var passphrase: Passphrase?
+        public var iv: String?
+        public var mac: String?
         
-        public init(algorithm: String, salt: String?=nil, iterations: Int?=nil, bits: Int?=nil) {
-            self.algorithm = algorithm
-            self.salt = salt
-            self.iterations = iterations
-            self.bits = bits
+        public struct Passphrase: Codable {
+            public var algorithm: String
+            public var salt: String?    // Required for m.pbkdf2
+            public var iterations: Int? // Required for m.pbkdf2
+            public var bits: Int?
+            
+            public init(algorithm: String, salt: String?=nil, iterations: Int?=nil, bits: Int?=nil) {
+                self.algorithm = algorithm
+                self.salt = salt
+                self.iterations = iterations
+                self.bits = bits
+            }
         }
+        
+        public init(name: String? = nil, algorithm: String, passphrase: Passphrase? = nil, iv: String? = nil, mac: String? = nil) {
+            self.name = name
+            self.algorithm = algorithm
+            self.passphrase = passphrase
+            self.iv = iv
+            self.mac = mac
+        }
+        
     }
-    
-    public init(name: String? = nil, algorithm: String, passphrase: Passphrase? = nil, iv: String? = nil, mac: String? = nil) {
-        self.name = name
-        self.algorithm = algorithm
-        self.passphrase = passphrase
-        self.iv = iv
-        self.mac = mac
-    }
-
 }
-
