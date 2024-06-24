e7185d2ff32e09c7e75b6652f7b90648d89cee9a
Charles Wright <cvwright@futo.org>
2024-06-24 15:05:31 -0500

    Add forgotten coding keys for Threepid

--- Sources/Matrix/Models/Matrix+Threepid.swift
+++ Sources/Matrix/Models/Matrix+Threepid.swift
@@ -13,5 +13,12 @@ extension Matrix {
         public var address: String
         public var medium: String
         public var validatedAt: UInt
+
+        public enum CodingKeys: String, CodingKey {
+            case addedAt = "added_at"
+            case address = "address"
+            case medium = "medium"
+            case validatedAt = "validated_at"
+        }
     }
 }
